5ad708e4d505441e544688d4e0f47d5be935065e
jonsax6 <jdc.saxjunky@gmail.com>
2021-11-02 13:03:08 -0400

    added responsive feature to Carousel

--- src/components/Home/CarouselComponent.js
+++ src/components/Home/CarouselComponent.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { indexArtwork } from '../../api/artwork'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Carousel } from 'react-responsive-carousel'
+import { useMediaQuery } from 'react-responsive'
+
+const desktop = '60%'
+const tablet = '75%'
+const phone = '90%'
 
 const CarouselComp = () => {
   const [cards, setCards] = useState([])
@@ -11,6 +16,24 @@ const CarouselComp = () => {
       setCards(res.data.artwork)
     })
   }, [])
+
+  const isPhone = useMediaQuery({
+    query: '(max-width: 600px)'
+  })
+
+  const isTablet = useMediaQuery({
+    query: '(max-width: 1000px)'
+  })
+
+  let mobileStyle
+
+  if (isPhone) {
+    mobileStyle = phone
+  } else if (isTablet) {
+    mobileStyle = tablet
+  } else {
+    mobileStyle = desktop
+  }
 
   return (
     <div style={{ marginTop: '50px' }} className={'demo-carousel'}>
@@ -24,7 +47,7 @@ const CarouselComp = () => {
         {cards.map((card) => {
           return (
             <div key={card._id}>
-              <img style={{ maxWidth: '60%' }} src={`${card.imageUrl}`} />
+              <img style={{ maxWidth: mobileStyle }} src={`${card.imageUrl}`} />
               {/* <p style={{ width: '300px', margin: 'auto', color: 'white', paddingTop: '30px' }}>{card.title}</p> */}
             </div>
           )
