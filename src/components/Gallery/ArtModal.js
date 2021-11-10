582e312527b367f67491eac9900523d1759a3d24
jonsax6 <jdc.saxjunky@gmail.com>
2021-11-10 00:00:56 -0500

    revised Carousel responsive design

--- src/components/Gallery/ArtModal.js
+++ src/components/Gallery/ArtModal.js
@@ -17,7 +17,7 @@ const desktop = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '50%',
+  width: '800px',
   bgcolor: 'background.paper',
   boxShadow: 24,
   p: 1
@@ -51,7 +51,7 @@ const ArtModal = ({ card, handleClose, open }) => {
   })
 
   const isTablet = useMediaQuery({
-    query: '(max-width: 1000px)'
+    query: '(max-width: 1200px)'
   })
 
   let mobileStyle
